8f94425e62d41b20faa7b781dcca4a95faa53b10
Jack Steinberg <jack.steinberg15@trinityschoolnyc.org>
2015-04-23 15:51:51 -0400

    Merge remote-tracking branch 'origin/dev-jack' into dev-jack
    Conflicts:
    	Gemfile
    	Gemfile.lock
    	app/assets/stylesheets/application.css.scss
    	app/views/layouts/application.html.erb
    	app/views/users/index.html.erb
    	config/application.rb

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
 CLIENT_ID = "nrjg1qo93yoz5a5unfxkyit810u794a"
-SECRET_KEY = "p9i50l9uqci72z1ffx4f8gtdiv25x89"
+SECRET_KEY = "iq0kgkle335y7teg0fn4p4a5e5kwy8r"
 REDIRECT_URI = "http://localhost:3000/auth/"
 SCOPES = ["user_read"]
