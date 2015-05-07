fafc75e69f7da826c65fe46d653e9730e2e3a021
Jack Steinberg <jack.steinberg15@trinityschoolnyc.org>
2015-05-07 19:27:01 -0400

    butn+solr
    +fixed solr error
    +fixed button clicks

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
 CLIENT_ID = "nrjg1qo93yoz5a5unfxkyit810u794a"
-SECRET_KEY = "p9i50l9uqci72z1ffx4f8gtdiv25x89"
+SECRET_KEY = "s1bwwdgytzaoq560pmmehwr9xnmqiuz"
 REDIRECT_URI = "http://localhost:3000/auth/"
 SCOPES = ["user_read"]
